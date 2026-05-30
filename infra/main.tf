b75b572edad76d6669a3f364b3762a0db8be90c6
tamilorea1 <tamiloreakinfemi@gmail.com>
2026-05-30 15:46:48 -0600

    Week 1 complete. Added a remote state(.tfstate) backend using an S3 bucket and Dynamo table. Had to ensure a bucket, bucket versioning, & a dynamo table is created first before the backend block inside terraform can be run

--- infra/main.tf
+++ infra/main.tf
@@ -9,6 +9,25 @@ terraform {
         version = "~> 5.0"
     }
   }
+
+  #Store the .tfstate file in an S3 bucket since it could be a single point of failure (SPOF)
+  backend "s3" {
+    # Which S3 bucket to store the state in
+    bucket = "terraform-state-tamilorea"
+
+    #The path inside that bucket (folder/file path on local machine)
+    key = "HoneyTokenMonitor/infra/terraform.tfstate"
+
+    #Where the bucket lives
+    #didn't use variable bacuase the backend runs during 'terraform init'. So it doesn't know it exists
+    region = "us-east-1"
+
+    #The table that handles locking
+    dynamodb_table = "terraform-lock"
+    
+    #Encrypts the state file at rest (should ALWAYS be done)
+    encrypt = true
+  }
 }
 
 /*
@@ -20,7 +39,36 @@ provider "aws" {
   region = var.aws_region 
 }
 
+resource "aws_s3_bucket" "bucket_for_tfstate" {
+  bucket = "terraform-state-tamilorea"
+}
+
+resource "aws_s3_bucket_versioning" "tfstate_versioning" {
+  #.id references the bucket 'terraform-state-tamilorea'
+  bucket = aws_s3_bucket.bucket_for_tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"  
+
+  #Primary key, unique identifier for each row
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+
+    #S menas String
+    type = "S"
+  }
+}
+
 resource "aws_iam_user" "test" {
   # name of our IAM user is 'test_user'
   name = var.test_user_name 
 }
+
